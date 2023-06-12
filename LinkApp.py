0db7cc734de9d973d4695abad7e33c7be4f9b8f1
shrivatsahosabettu <shrivatsa555@gmail.com>
2023-06-12 11:36:55 +0530

    Added feature of colored buttons

--- LinkApp.py
+++ LinkApp.py
@@ -5,6 +5,7 @@ from settings import *
 from PIL import Image, ImageTk
 from CTkMessagebox import CTkMessagebox
 from dyn_button import *
+from tkcolorpicker import askcolor
 
 try:
     from ctypes import windll, byref, sizeof, c_int
@@ -18,7 +19,12 @@ except:
 class LinkApp(ctk.CTk):
     def __init__(self, is_dark):
         super().__init__(fg_color=(WHITE, BG_COLOR))
-        ctk.set_appearance_mode(f'{"dark" if is_dark else "light"}')
+        ctk.set_appearance_mode('system')
+        # ctk.set_appearance_mode(f'{"dark" if is_dark else "light"}')
+        if darkdetect.theme() == 'Dark':
+            self.display_mode = 'dark'
+        else:
+            self.display_mode = 'light'
         self.geometry(f'{APP_SIZE[0]}x{APP_SIZE[1]}')
         self.minsize(*map(int, '730x600'.split('x')))
         self.title('')
@@ -32,6 +38,7 @@ class LinkApp(ctk.CTk):
         # Variables
         self.add_var = ctk.StringVar()
         self.button_name_var = ctk.StringVar()
+        self.button_color_var = ctk.StringVar()
         self.button_link_var = ctk.StringVar()      
         self.font_size = int(FONT[1])
         self.height = int(self.font_size)
@@ -41,15 +48,21 @@ class LinkApp(ctk.CTk):
         self.button_name_link_list = []
         self.next_button_num = 1
         self.edit_button_index = None
+        self.color_choosen=(WHITE, BG_COLOR)
         # Initial settings
         if os.path.exists(self.file_path):
             self.reload_button_list()
         # widgets
         self.create_frames()
         self.call_all_widgets()
+        self.has_three_entries = False
         # To check if the file is already exists then load all the buttons
         # from the file
         for init_button_counter in range(len(self.button_name_link_list)):
+            if len(self.button_name_link_list[init_button_counter]) > 2:
+                self.has_three_entries = True
+            else:
+                self.has_three_entries = False
             self.initial_create_button(init_button_counter)
         self.mainloop()
         
@@ -95,7 +108,7 @@ class LinkApp(ctk.CTk):
         # Logo Image
         self.label_header = ctk.CTkLabel(self, text="Application Links", font=('Roboto', 25), bg_color=(WHITE, BG_COLOR))   
         self.label_header.grid(row=0, column=0, sticky='n', padx=10, pady=5)
-        self.label_note = ctk.CTkLabel(self, text="Note: Right click to delete the button\n ctrl+click to edit\n click to open the webpage", font=('Roboto', 12, 'bold'), bg_color=(WHITE, BG_COLOR)) 
+        self.label_note = ctk.CTkLabel(self, text="Note: Right click to Edit the button\n ctrl+click to Delete\n click to open the webpage", font=('Roboto', 12, 'bold'), bg_color=(WHITE, BG_COLOR)) 
         self.label_note.grid(row=0, column=0, sticky='w', padx=10, pady=5)   
  
         
@@ -128,7 +141,7 @@ class LinkApp(ctk.CTk):
         
         self.button_name_text = ctk.CTkEntry(
             self.additional_frame, 
-            width=400, 
+            width=350, 
             height=self.height, 
             textvariable=self.button_name_var,
             font=FONT, 
@@ -137,7 +150,29 @@ class LinkApp(ctk.CTk):
             corner_radius=5,
             border_color=HIGHLIGHT_COLOR,
             border_width=1)
+
         self.button_name_text.grid(row=0, column=1, sticky='w', padx=5, pady=5)
+        # Button Color
+        self.button_color_text = ctk.CTkEntry(
+            self.additional_frame, 
+            width=100, 
+            height=self.height, 
+            textvariable=self.button_color_var,
+            font=FONT, 
+            bg_color=(WHITE, BG_COLOR), 
+            fg_color=(WHITE, BG_COLOR),
+            corner_radius=5,
+            border_color=HIGHLIGHT_COLOR,
+            border_width=1)
+
+        self.color_chooser_button = ctk.CTkButton(
+            self.additional_frame, 
+            text='Choose Color', 
+            font=FONT, 
+            bg_color=(WHITE, BG_COLOR), 
+            command=self.choose_color
+            )
+        self.color_chooser_button.grid(row=0, column=1, sticky='e', padx=250, pady=5)
         # Button Link
         button_link_label = ctk.CTkLabel(
             self.additional_frame, 
@@ -182,6 +217,7 @@ class LinkApp(ctk.CTk):
         if self.add_var.get() == 'on':
             self.button_name_var.set('')
             self.button_link_var.set('')
+            self.button_color_var.set('')
             self.additional_frame.grid(row=2, column=0, sticky='nsew', padx=15, pady=5)
             self.button_name_text.focus()
             self.button_name_text.icursor(ctk.END)
@@ -195,35 +231,55 @@ class LinkApp(ctk.CTk):
     def initial_create_button(self, init_button_counter):
         init_new_button = Button(self.middle_frame, text=self.button_name_link_list[init_button_counter][0],
                                 row=len(self.button_list)//5, col=len(self.button_list)%5)
+        
+        if self.has_three_entries:
+            init_new_button.configure(fg_color=tuple(self.button_name_link_list[init_button_counter][2]),
+                                      hover_color=tuple(self.button_name_link_list[init_button_counter][3]))
+        self.display_mode_check()
+        init_new_button.configure(text_color=self.text_color)
         self.button_list.append(init_new_button)
         self.next_button_num += 1
-        init_new_button.bind("<Button-3>", lambda event, button=init_new_button: self.handle_right_click(button))     
+        init_new_button.bind("<Button-3>", lambda event, button=init_new_button: self.handle_control_click_delete(button))     
         init_new_button.bind("<Button-1>", lambda event, button=init_new_button: self.web_launcher(button))
-        init_new_button.bind("<Control-Button-1>", lambda event, button=init_new_button: self.handle_control_click_edit(button))
+        init_new_button.bind("<Control-Button-1>", lambda event, button=init_new_button: self.handle_right_click(button))
     # To create new button after adding the information in the bottom frame
     def create_button(self, button_index = None):
         if self.button_name_text.get() != '' and self.button_link_text.get() != '' and button_index == None:
-            new_button = Button(self.middle_frame, text=self.button_name_text.get(),
-                                row=len(self.button_list)//5, col=len(self.button_list)%5)
-            self.button_list.append(new_button)
-            self.button_info = [self.button_name_text.get(), self.button_link_text.get()]
-            self.button_name_link_list.append(self.button_info)
-            self.next_button_num += 1
-            self.hide_bottom_frame()
-            self.write_button_list()
-            new_button.bind("<Button-3>", lambda event, button=new_button: self.handle_right_click(button))     
-            new_button.bind("<Button-1>", lambda event, button=new_button: self.web_launcher(button))
-            new_button.bind("<Control-Button-1>", lambda event, button=new_button: self.handle_control_click_edit(button))
+            if self.button_color_var.get() == "":
+                self.show_color_blank_error() 
+            else:   
+                new_button = Button(self.middle_frame, text=self.button_name_text.get(),
+                                    row=len(self.button_list)//5, col=len(self.button_list)%5)
+                self.display_mode_check()
+                new_button.configure(fg_color=self.color_choosen, text_color=self.text_color, hover_color=self.color_choosen_hover)
+                self.button_list.append(new_button)
+                self.button_info = [self.button_name_text.get(), self.button_link_text.get(), list(self.color_choosen), list(self.color_choosen_hover)]
+                self.button_name_link_list.append(self.button_info)
+                self.next_button_num += 1
+                self.hide_bottom_frame()
+                self.write_button_list()
+                new_button.bind("<Button-3>", lambda event, button=new_button: self.handle_control_click_delete(button))     
+                new_button.bind("<Button-1>", lambda event, button=new_button: self.web_launcher(button))
+                new_button.bind("<Control-Button-1>", lambda event, button=new_button: self.handle_right_click(button))
         elif self.button_name_text.get() != '' and self.button_link_text.get() != '' and button_index != None:
             self.update_button_text(self.ctrl_button)
             self.button_name_link_list[button_index][0] = self.button_name_text.get()
             self.button_name_link_list[button_index][1] = self.button_link_text.get()
+            if self.button_color_var.get() == '':
+                self.color_choosen = (WHITE, BG_COLOR)
+            else:
+                if self.edit_button_index != None and len(self.button_name_link_list[self.edit_button_index]) > 2:
+                    self.button_name_link_list[button_index][2] = list(self.color_choosen)
+                    self.button_name_link_list[button_index][3] = list(self.color_choosen_hover)
+                else:
+                    self.button_name_link_list[button_index].insert(2, list(self.color_choosen))
+                    self.button_name_link_list[button_index].insert(3, list(self.color_choosen_hover))
             self.additional_frame.grid_forget()
             self.write_button_list()
         else:
-            self.show_button_blank_error()    
+            self.show_button_blank_error()   
 
-    # Functionality of right click
+    # Functionality of right click      - For Edit
     def handle_right_click(self, button):
         self.show_warning()
         if self.msg.get() == 'Ok':
@@ -243,12 +299,70 @@ class LinkApp(ctk.CTk):
             # Write the new button list to the file
             self.write_button_list()
             
-    # Functionality of control click
-    def handle_control_click_edit(self, button):
+    # Color chooser function
+    def choose_color(self):
+        # Display a color chooser dialog box and get the chosen color
+        color_tuple = askcolor()
+        # Get the hexadecimal value of the chosen color
+        if color_tuple[1] == None:
+            self.show_color_blank_error()
+        else:
+            self.fg_color_hex = color_tuple[1]
+            # Set the Entry widget's text to the chosen color's hexadecimal value
+            self.button_color_text.delete(0, tk.END)
+            self.button_color_text.insert(0, self.fg_color_hex)
+            self.prepare_color(self.fg_color_hex)
+
+    # Prepare the color chooser based on the display mode
+    def prepare_color(self, color):
+        base_color_hex = color 
+        # Get the 10% lighter color
+        self.lighter_or_dark_color_hex = self.get_dark_light_color(base_color_hex)
+        self.lighter_or_dark_color_hex_hover = self.get_dark_light_color_hover(base_color_hex)
+        if self.display_mode == 'dark':
+            self.color_choosen = (self.lighter_or_dark_color_hex, base_color_hex)
+            self.color_choosen_hover = (base_color_hex, self.lighter_or_dark_color_hex_hover)
+        else:
+            self.color_choosen =  (base_color_hex, self.lighter_or_dark_color_hex)
+            self.color_choosen_hover = (self.lighter_or_dark_color_hex_hover, base_color_hex)
+
+    # Helper function to get the dark or light color
+    def get_dark_light_color(self, hex_color):
+        rgb_color = tuple(int(hex_color[i:i+2], 16) for i in (1, 3, 5))
+        if self.display_mode == 'dark':
+            darker_rgb = tuple(min(round(c * 0.5), 255) for c in rgb_color)
+            darker_hex = '#{:02x}{:02x}{:02x}'.format(*darker_rgb)
+            return darker_hex
+        else:
+            lighter_rgb = tuple(min(round(c * 1.5), 255) for c in rgb_color)
+            lighter_hex = '#{:02x}{:02x}{:02x}'.format(*lighter_rgb)
+            return lighter_hex
+
+    # Helper function to get the dark or light color for hover
+    def get_dark_light_color_hover(self, hex_color):
+        rgb_color = tuple(int(hex_color[i:i+2], 16) for i in (1, 3, 5))
+
+        if self.display_mode == 'dark':
+            darker_rgb_hover = tuple(min(round(b / 2), 255) for b in rgb_color)
+            darker_hex_hover = '#{:02x}{:02x}{:02x}'.format(*darker_rgb_hover)
+            return darker_hex_hover
+        else:
+            lighter_rgb_hover = tuple(min(round(c /25) + 50, 255) for c in rgb_color)
+            lighter_hex_hover = '#{:02x}{:02x}{:02x}'.format(*lighter_rgb_hover)
+            return lighter_hex_hover
+
+        
+        
+    # Functionality of control click        - For delete
+    def handle_control_click_delete(self, button):
         self.ctrl_button = button
         self.edit_button_index = self.button_list.index(button)
         self.button_name_var.set(self.button_name_link_list[self.edit_button_index][0])
         self.button_link_var.set(self.button_name_link_list[self.edit_button_index][1])
+        if len(self.button_name_link_list[self.edit_button_index]) > 2:
+            self.button_color_var.set(self.button_name_link_list[self.edit_button_index][2])
+        else:
+            self.color_choosen
         self.button_name_text.bind("<<Modified>>", lambda: self.update_button_text(button))
         self.additional_frame.grid(row=2, column=0, sticky='nsew', padx=15, pady=5)
     
@@ -256,7 +370,13 @@ class LinkApp(ctk.CTk):
     def update_button_text(self, button):
         new_text = self.button_name_text.get()
         new_link = self.button_link_text.get()
+        if self.button_color_var.get() == "":
+            new_color = self.color_choosen
+        else:
+            new_color = self.button_color_var.get()
         button.configure(text=new_text)
+        button.configure(fg_color=new_color)
+        self.prepare_color(new_color)
         self.edit_button_index = None
     
     # Write button to the file
@@ -267,12 +387,20 @@ class LinkApp(ctk.CTk):
     # Show warning message before deleting the button     
     def show_warning(self):
     # Show some retry/cancel warnings
+        self.display_mode_check()
         self.msg = CTkMessagebox(title="Delete Button!", message="Do you want to delete this button?!",
-                  icon="warning", option_1="Cancel", option_2="Ok", topmost=True)
+                  icon="warning", option_1="Cancel", option_2="Ok", topmost=True, text_color=self.text_color)
     
     # Show error message if the bottom frame widgets are blank
     def show_button_blank_error(self):
-        CTkMessagebox(title="Error", message="Please fill in all fields!!!", icon="cancel", fg_color=BG_COLOR, bg_color=BG_COLOR, topmost=True)            
+        self.display_mode_check_color()
+        CTkMessagebox(title="Error", message="Please fill in all fields!!!", icon="cancel", fg_color=BG_COLOR, bg_color=BG_COLOR, topmost=True,text_color=self.text_color)            
+    
+    # color not choosen error
+    def show_color_blank_error(self):
+        self.display_mode_check_color()
+        CTkMessagebox(title="Error", message="Please Select the color of the button!!!", icon="cancel", fg_color=BG_COLOR, bg_color=BG_COLOR, topmost=True, text_color=self.text_color)  
+    
     
     # Web page open based on the button click
     def web_launcher(self, button):
@@ -285,7 +413,16 @@ class LinkApp(ctk.CTk):
         with open(self.file_path, 'r') as f:
             json_button_string = f.read()
             if json_button_string != '':
-                self.button_name_link_list =json.loads(json_button_string)     
+                self.button_name_link_list =json.loads(json_button_string)   
+
+    def display_mode_check(self):
+        self.text_color = ("black", "white")
+                
+    def display_mode_check_color(self):
+        if self.display_mode == 'dark':
+            self.text_color = ("black", "white")
+        else:
+            self.text_color = ("white", "black")
     # To handle the title bar color to match the frame body               
     def title_bar_color(self, is_dark):
             try:
